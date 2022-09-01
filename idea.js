862f3b0ac5e61534832eff82c28f874070f2feb9
ForceBlood <barkinkoral@hotmail.com>
2022-09-01 18:20:16 +0300

    Update idea.js and add the resulting encrypted files

--- idea.js
+++ idea.js
@@ -6,6 +6,12 @@ const key = crypto.randomBytes(128).toString('hex'); // Generate 128 bit long ke
 
 const cipher = new IDEA(key)
 
+function buf2hex(buffer) { // buffer is an ArrayBuffer
+    return [...new Uint8Array(buffer)]
+        .map(x => x.toString(16).padStart(2, '0'))
+        .join('');
+  }
+
 function timeDiff(start, end) {
     return end - start;
 }
@@ -13,7 +19,7 @@ function timeDiff(start, end) {
 const start = new Date();
 for (i = 1; i <= 1000; i++) {
 let encrypted = cipher.encrypt('Hello World')
-fs.appendFileSync('ideaencrypted.txt', encrypted.toString('hex'));
+fs.appendFileSync('ideaencrypted.txt', buf2hex(encrypted.buffer));
 let decrypted = cipher.decrypt(encrypted)
 }
 const end = new Date();
